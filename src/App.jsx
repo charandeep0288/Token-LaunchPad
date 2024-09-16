3b260c7995b83f880210d854ac5a522cc9f9a072
charandeep0288 <ckaunsel123@gmail.com>
2024-09-16 20:50:18 +0530

    Added the solana Wallet Adapter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import "./App.css";
 import { TokenLaunchpad } from "./components/TokenLaunchpad";
 
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import '@solana/wallet-adapter-react-ui/styles.css';
+
 function App() {
   return (
-    <TokenLaunchpad />
+    <div>
+      <ConnectionProvider endpoint="https://api.devnet.solana.com">
+        <WalletProvider wallets={[]} autoConnect>
+          <WalletModalProvider>
+            <div style={{ width: '95vw', display: 'flex', justifyContent: 'space-between', padding: 20}}>
+              <WalletMultiButton />
+              <WalletDisconnectButton />
+            </div>
+              <TokenLaunchpad />
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </div>
   );
 }
 
